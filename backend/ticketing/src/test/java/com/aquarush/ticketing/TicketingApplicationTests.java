c89929e7e0eb1212a21eea43ad077e90f5b453f7
ella.oh <skfsk1602@gmail.com>
2025-10-16 18:26:26 +0900

    #3 feat: Spring boot 초기 설정

---
+++ backend/ticketing/src/test/java/com/aquarush/ticketing/TicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquarush.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
